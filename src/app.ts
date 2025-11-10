b8b9e27ee9e0ac144dd16aff14bc35ae7d4d1353
Simon Balanoff <sjbalanoff@gmail.com>
2025-11-09 23:20:11 -0700

    Updated namespaces for Vercel

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express"
-import helmet from "helmet"
-import rateLimit from "express-rate-limit"
+import * as helmet from "helmet"
+import * as rateLimit from "express-rate-limit"
 import { env } from "./env.js"
 import { connectDb } from "./db.js"
 import { corsMiddleware } from "./middleware/cors.js"
@@ -10,13 +10,24 @@ import { residentsRouter } from "./routes/residents.js"
 import { assessmentsRouter } from "./routes/assessments.js"
 
 const app = express()
+
 app.set("x-powered-by", false)
-app.use(helmet())
+app.use(helmet.default())
 app.use(corsMiddleware)
 app.use(express.json({ limit: "1mb" }))
 
-const generalLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: env.RATE_GENERAL_MAX, standardHeaders: true, legacyHeaders: false })
-const authLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: env.RATE_AUTH_MAX, standardHeaders: true, legacyHeaders: false })
+const generalLimiter = rateLimit.default({
+  windowMs: 15 * 60 * 1000,
+  max: env.RATE_GENERAL_MAX,
+  standardHeaders: true,
+  legacyHeaders: false
+})
+const authLimiter = rateLimit.default({
+  windowMs: 15 * 60 * 1000,
+  max: env.RATE_AUTH_MAX,
+  standardHeaders: true,
+  legacyHeaders: false
+})
 
 app.use(generalLimiter)
 app.use("/auth", authLimiter)
@@ -29,5 +40,4 @@ app.use(notFound)
 app.use(errorHandler)
 
 await connectDb()
-
 export default app
